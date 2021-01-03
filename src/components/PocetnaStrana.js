210c86413d09eeee1afe50a1437a6894c9d33d40
sinisamali <simke.vgv@gmail.com>
2021-01-03 12:27:54 +0100

    React forma 2-way binding

--- src/components/PocetnaStrana.js
+++ src/components/PocetnaStrana.js
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Btn from './Btn';
 import Tabela from './Tabela';
 
 const PocetnaStrana = (props) => {
+  const [poStrani, setPoStrani] = useState("5");
+
+  const handleChange = e => {
+    // ova funkcija se poziva svaki put kada korisnik promeni nesto u odredjenom polju forme (onChange event)
+
+    let value = e.target.value; // value koji je korisnik izabrao
+    console.log(typeof value);
+    setPoStrani(value);
+
+  };
+
+
+
   return (
     <div className="pocetna-strana" >
       <div className="toolBar">
@@ -10,14 +23,18 @@ const PocetnaStrana = (props) => {
         <span>Strana: 1</span>
         <Btn title="previous" />
 
-        <label>Po strani:</label>
-        <select value="5">
-        <option value="">--IZABERI OPCIJU--</option>
-          <option value="3">3</option>
-          <option value="5">5</option>
-          <option value="10">10</option>
-          <option value="15">15</option>
-        </select>
+        <label>Po strani:
+        <select
+            value={poStrani}
+            onChange={handleChange}
+          >
+            <option value="">--IZABERI OPCIJU--</option>
+            <option value="3">3</option>
+            <option value="5">5</option>
+            <option value="10">10</option>
+            <option value="15">15</option>
+          </select>
+        </label>
 
       </div>
       <Tabela />

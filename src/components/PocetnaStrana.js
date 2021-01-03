45c125b6c61be20e999c0f2e2480844e2e76a582
sinisamali <simke.vgv@gmail.com>
2021-01-03 12:11:08 +0100

    Toolbar spreman (dizajniran) za funkcionalnost

--- src/components/PocetnaStrana.js
+++ src/components/PocetnaStrana.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import Btn from './Btn';
 import Tabela from './Tabela';
 
 const PocetnaStrana = (props) => {
   return (
     <div className="pocetna-strana" >
+      <div className="toolBar">
+        <Btn title="next" />
+        <span>Strana: 1</span>
+        <Btn title="previous" />
+
+        <label>Po strani:</label>
+        <select value="5">
+        <option value="">--IZABERI OPCIJU--</option>
+          <option value="3">3</option>
+          <option value="5">5</option>
+          <option value="10">10</option>
+          <option value="15">15</option>
+        </select>
+
+      </div>
       <Tabela />
     </div>
   );

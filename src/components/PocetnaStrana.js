d65d50b580c2f836e019f204480167ab641f13b2
sinisamali <simke.vgv@gmail.com>
2021-01-20 11:51:53 +0100

    Prebaceno strana i po strani u globalni redux state i proradio prvi reducer

--- src/components/PocetnaStrana.js
+++ src/components/PocetnaStrana.js
@@ -1,27 +1,56 @@
 import React, { useState } from 'react';
+import { connect } from 'react-redux';
 import Btn from './Btn';
 import Tabela from './Tabela';
 
 const PocetnaStrana = (props) => {
-  const [poStrani, setPoStrani] = useState("5");
+  // const [poStrani, setPoStrani] = useState("5");
+  // const [strana, setStrana] = useState(1);
+  let strana = props.strana;
+  let poStrani = props.poStrane
 
   const handleChange = e => {
     // ova funkcija se poziva svaki put kada korisnik promeni nesto u odredjenom polju forme (onChange event)
 
     let value = e.target.value; // value koji je korisnik izabrao
     console.log(typeof value);
-    setPoStrani(value);
+    // setPoStrani(value);
 
   };
+
+  const nextPage = () => {
+    console.log('kliknuli smo')
+    /*
+    dispatchovqanjem govorimo reduxu sta zelimo da izmeni u stateu
+    kao argument dispatch funkcije se stavlja action
+    Action je u formatu FLUX STANDARD ACTION https://github.com/redux-utilities/flux-standard-action
+
+    */
+    props.dispatch({
+      type: 'NEXT_PAGE'
+    })
+
+  }
+  const prevPage = () => {
+    props.dispatch({
+      type: 'PREV_PAGE'
+    })
+  }
 
 
 
   return (
     <div className="pocetna-strana" >
       <div className="toolBar">
-        <Btn title="next" />
-        <span>Strana: 1</span>
-        <Btn title="previous" />
+        <Btn
+          title="next"
+          handleClick={nextPage}
+        />
+        <span>Strana: {strana}</span>
+        <Btn
+          title="previous"
+          handleClick={prevPage}
+        />
 
         <label>Po strani:
         <select
@@ -37,9 +66,17 @@ const PocetnaStrana = (props) => {
         </label>
 
       </div>
-      <Tabela poStrani={poStrani} />
+      <Tabela poStrani={poStrani} strana={strana} />
     </div>
   );
 }
+// export default PocetnaStrana;
 
-export default PocetnaStrana;
+// POVEZIVANJE SA REDUX STATEOM APLIKACIJE I REDUX DISPATCHEROM
+const mapStateToProps = state => ({
+  readme: 'Sve propertije iz ovog objekta komponenta iznad prima kao props + i props.dispatch',
+  strana: state.strana,
+  poStrane: state.poStrani
+});
+
+export default connect(mapStateToProps)(PocetnaStrana); // ovom se formira specijalan komponenta iznad nase, koja sluzi da nasoj komponenti posalje props koje smo definisali u mapStateToProps
--- src/redux/app-reducer.js
+++ src/redux/app-reducer.js
@@ -1,55 +1,57 @@
 
 
-  const adresarData = [
-    {
-      id: 1,
-      name: 'Zoran',
-      prezime: 'Markovic',
-      godine: 25
-    },
-    {
-      id: 2,
-      name: 'Marko',
-      prezime: 'Markovic',
-      godine: 24
-    },
-    {
-      id: 3,
-      name: 'Jovan',
-      prezime: 'Markovic',
-      godine: 23
-    },
-    {
-      id: 4,
-      name: 'Srdjan',
-      prezime: 'Markovic',
-      godine: 22
-    },
-    {
-      id: 5,
-      name: 'Miodrag',
-      prezime: 'Markovic',
-      godine: 60
-    },
-    {
-      id: 6,
-      name: 'Lazar',
-      prezime: 'Markovic',
-      godine: 40
-    },
-    {
-      id: 7,
-      name: 'Fiona',
-      prezime: 'Markovic',
-      godine:50
-    }
-  ];
+const adresarData = [
+  {
+    id: 1,
+    name: 'Zoran',
+    prezime: 'Markovic',
+    godine: 25
+  },
+  {
+    id: 2,
+    name: 'Marko',
+    prezime: 'Markovic',
+    godine: 24
+  },
+  {
+    id: 3,
+    name: 'Jovan',
+    prezime: 'Markovic',
+    godine: 23
+  },
+  {
+    id: 4,
+    name: 'Srdjan',
+    prezime: 'Markovic',
+    godine: 22
+  },
+  {
+    id: 5,
+    name: 'Miodrag',
+    prezime: 'Markovic',
+    godine: 60
+  },
+  {
+    id: 6,
+    name: 'Lazar',
+    prezime: 'Markovic',
+    godine: 40
+  },
+  {
+    id: 7,
+    name: 'Fiona',
+    prezime: 'Markovic',
+    godine: 50
+  }
+];
 
 
 
 const initialState = {
   nesto: 'blabla',
-  adresar: adresarData
+  adresar: adresarData,
+  strana: 1,
+  poStrani: 5
 }
 
 // Use the initialState as a default value
@@ -57,10 +59,24 @@ export default function appReducer(state = initialState, action) {
   // The reducer normally looks at the action type field to decide what happens
   switch (action.type) {
     // Do something here based on the different types of actions
+
+    case 'NEXT_PAGE':
+      return {
+        ...state,
+        strana: state.strana + 1
+      };
+
+    case 'PREV_PAGE':
+      return {
+        ...state,
+        strana: state.strana - 1
+      };
+
     default:
       // If this reducer doesn't recognize the action type, or doesn't
       // care about this specific action, return the existing state unchanged
       return state
   }
 }
+
 
